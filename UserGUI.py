79ce06e10e5116a94de6ac0319161ff96211dd34
Ben <bjh1381@rit.edu>
2018-11-05 14:38:27 -0500

    Added a scroll bar to GUI, that scrolls down automatically when a new command is entered.

--- UserGUI.py
+++ UserGUI.py
@@ -10,12 +10,17 @@ class GUI:
 
         self.output = Text(root)
         self.output.configure(state="disabled")
-        self.entry = Entry(root, )
+
+        scroll = Scrollbar(root, command=self.output.yview)
+        self.output['yscrollcommand'] = scroll.set
+
+        self.entry = Entry(root)
         enter = Button(root, text="Enter", command=self.submit)
 
         self.output.grid(row=0, column=0, columnspan=3, padx=10, sticky=E + W)
         self.entry.grid(row=1, column=1, padx=0, pady=10, sticky=E + W)
-        enter.grid(row=1, column=2, padx=10, pady=10, sticky=E+W)
+        scroll.grid(row=0, column=3, padx=5, pady=10, sticky=N + S)
+        enter.grid(row=1, column=2, columnspan=2, padx=10, pady=10, sticky=E+W)
         root.grid_columnconfigure(1, weight=1)
         root.grid_rowconfigure(0, weight=1)
 
@@ -25,6 +30,7 @@ class GUI:
         if self.entry.get() != "":
             self.terminal_write(self.entry.get())
             self.entry.delete(0, 'end')
+            self.output.yview_moveto(1)
 
 
     def terminal_write(self, text):
