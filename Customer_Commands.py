9f75b295b22a9d64510778f08c738165b236a4da
Jacob Potter <jmp1617@g.rit.edu>
2018-12-01 15:16:24 -0500

    stubs for user commands

---
+++ Customer_Commands.py
@@ -0,0 +1,18 @@
+def place_order():
+    return
+
+
+def accept_charge():
+    return
+
+
+def list_orders():
+    return
+
+
+def track_package():
+    return
+
+
+def bill_status():
+    return
---
+++ Employee_Commands.py
@@ -0,0 +1,19 @@
+def list_packages():
+    return
+
+
+def update_hold_loc():
+    return
+
+
+def mark_in_transit():
+    return
+
+
+def mark_as_delivered():
+    return
+
+
+def change_expected_delivery():
+    return
+
