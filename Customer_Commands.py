9ec2c1c5c964874d9d3a94452b677e1e43af968e
Jacob Potter <jmp1617@g.rit.edu>
2018-12-01 16:03:54 -0500

    employee command complete

--- Customer_Commands.py
+++ Customer_Commands.py
@@ -7,28 +7,27 @@ active_charge = 0
 def place_order(type, weight, source_addr, destination_addr):
     cost_for_delivery = (weight*51)/100
     active_charge = cost_for_delivery
-    pd.execute_command()
+    pd.execute_command("")
     return cost_for_delivery
 
 
 def accept_charge(payment_type):
-    pd.execute_command()
+    pd.execute_command("")
     return "Charge of " + str(active_charge) + " accepted"
 
 
 def list_orders():
-    pd.execute_command()
-    pass
+    pd.execute_command("")
 
 
 def track_package(tracking_number):
     location = ""
     expected_delivery = ""
-    pd.execute_command()
+    pd.execute_command("")
     return location, expected_delivery
 
 
 def bill_status():
     total = 0
-    pd.execute_command()
+    pd.execute_command("")
     return "Bill total: " + str(total)
--- Employee_Commands.py
+++ Employee_Commands.py
@@ -1,19 +1,22 @@
+import PackageDatabase as pd
+
+
 def list_packages():
-    return
+    pd.execute_command("")
 
 
-def update_hold_loc():
-    return
+def update_hold_loc(package, location):
+    pd.execute_command("")
 
 
-def mark_in_transit():
-    return
+def mark_in_transit(package, vehicle):
+    pd.execute_command("")
 
 
-def mark_as_delivered():
-    return
+def mark_as_delivered(package):
+    pd.execute_command("")
 
 
-def change_expected_delivery():
-    return
+def change_expected_delivery(package, date):
+    pd.execute_command("")
 
