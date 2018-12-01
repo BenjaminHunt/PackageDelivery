98f789671ad28a2ab9b5f3272c1d23ec56b31a8c
Jacob Potter <jmp1617@g.rit.edu>
2018-12-01 15:58:39 -0500

    customer commands complete

--- Customer_Commands.py
+++ Customer_Commands.py
@@ -7,25 +7,28 @@ active_charge = 0
 def place_order(type, weight, source_addr, destination_addr):
     cost_for_delivery = (weight*51)/100
     active_charge = cost_for_delivery
-
+    pd.execute_command()
     return cost_for_delivery
 
 
 def accept_charge(payment_type):
-    command = ""
-    return command
+    pd.execute_command()
+    return "Charge of " + str(active_charge) + " accepted"
 
 
 def list_orders():
-    command = ""
-    return command
+    pd.execute_command()
+    pass
 
 
-def track_package():
-    command = ""
-    return command
+def track_package(tracking_number):
+    location = ""
+    expected_delivery = ""
+    pd.execute_command()
+    return location, expected_delivery
 
 
 def bill_status():
-    command = ""
-    return command
+    total = 0
+    pd.execute_command()
+    return "Bill total: " + str(total)
