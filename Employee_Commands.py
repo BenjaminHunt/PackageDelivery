65ef76c00075bb7cc4341ca0120c4552d195c236
Jacob Potter <jmp1617@g.rit.edu>
2018-12-04 17:17:41 -0500

    added validity check to employee

--- Employee_Commands.py
+++ Employee_Commands.py
@@ -3,21 +3,40 @@ import PackageDatabase as pd
 
 def list_packages():
     valid = True
-    return valid, pd.execute_command("SELECT * FROM package")
+    result = pd.execute_command("SELECT * FROM package")
+    if result == "Invalid SQL":
+        valid = False
+    return valid, result
 
 
 def update_hold_loc(package, location):
-    pd.execute_command("")
+    valid = True
+    result = pd.execute_command("UPDATE package SET location={} WHERE id={}".format(location, package))
+    if result == "Invalid SQL":
+        valid = False
+    return valid
 
 
 def mark_in_transit(package, vehicle):
-    pd.execute_command("")
+    valid = True
+    result = pd.execute_command("")
+    if result == "Invalid SQL":
+        valid = False
+    return valid
 
 
 def mark_as_delivered(package):
-    pd.execute_command("")
+    valid = True
+    result = pd.execute_command("")
+    if result == "Invalid SQL":
+        valid = False
+    return valid
 
 
 def change_expected_delivery(package, date):
-    pd.execute_command("")
+    valid = True
+    result = pd.execute_command("")
+    if result == "Invalid SQL":
+        valid = False
+    return valid
 
